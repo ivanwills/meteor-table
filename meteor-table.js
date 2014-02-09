bccb3cdd1ca50b8549b65378d6f426bf93a22669
Ivan Wills <ivan.wills@gmail.com>
2014-02-09 20:52:15 +1100

    Got table data displaying

--- meteor-table.js
+++ meteor-table.js
@@ -1,15 +1,9 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to meteor-table.";
-  };
+var users = new Meteor.Collection("users");
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+if (Meteor.isClient) {
+    Template.table.rows = function() {
+        return users.find({}, {limit: 10});
     }
-  });
 }
 
 if (Meteor.isServer) {
